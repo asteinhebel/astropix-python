0ef5aac6cdf9a17463ee908254fe1713b7d9ad64
Amanda Steinhebel <asteinhebel151@gmail.com>
2022-05-08 11:56:14 -0500

    Save array of masked pixels to txt file for use in data analysis (to determine masked vs dead pixels in beam)

--- noiseVisualization.py
+++ noiseVisualization.py
@@ -31,11 +31,14 @@ def saveFromInput(saveFile):
 		
 def constructMasks(counts,high):
 
+	maskArr=np.zeros((35,35)) #filler value
+
 	for col in range(len(counts)):
 		bits="0" #Every one should end in 0 
 		for row in range(len(counts[0])):
 			if counts[col][row]>high:
 				bits+="1"#mask
+				maskArr[col][row]=-1
 			else:
 				bits+="0"#leave on
 		#Add '_' delimeters 
@@ -43,17 +46,19 @@ def constructMasks(counts,high):
 		#invert order
 		bits=bits[::-1]
 		print(f"self.recconfig[f'ColConfig{col}'] = 0b00{bits}")
-	return 0
+		#print(f"self.reconfig={{'ColConfig{col}':0b00{bits}}}")
+
+	return maskArr
 
 if __name__ == "__main__":
 
 	#########################
 	## variables you may want to change
-	loc="bench"
-	#loc="cavern"
+	#loc="bench"
+	loc="cavern"
 	dataDir=f"output_{loc}/"
 	saveto=f"noiseMap_{loc}.pdf"
-	threshold=100
+	threshold=0
 	#########################
 
 
@@ -63,7 +68,7 @@ if __name__ == "__main__":
 	nmbs=['0','1','2','3','4','5','6','7','8','9']
 	
 	os.chdir(dataDir)	
-	for f in sorted(glob.glob("*.txt")): #get all txt files in datadir in alphabetic order ('sorted' alphabatizes)
+	for f in sorted(glob.glob("lognoise*.txt")): #get all txt files in datadir in alphabetic order ('sorted' alphabatizes)
 		colname=f.split('_')[1]
 		if colname[-2] in nmbs: #if two digit column
 			colVal=int(colname[-2])*10+int(colname[-1])
@@ -85,20 +90,31 @@ if __name__ == "__main__":
 	cbar.set_label('Counts (of 1000)') 	
 	plt.tight_layout() #reduce margin space	
 	
-	#plt.show()
-	saveFromInput(saveto)
+	plt.show()
+	#saveFromInput(saveto)
 	
 	#Create masking strings
 	print("Create Masking Strings")
-	constructMasks(counts,threshold)
+	maskArr=constructMasks(counts,threshold)
 	
 	fullArray=35*totalCols
 	counts=sum(counts,[])#flatten list
 	noisy=0
+	noisyish=0
 	for c in counts:
 		if c>threshold:
-				noisy+=1
+			noisy+=1
+		elif c>0:
+			noisyish+=1
 	
+	print(f"Total Pixels: {fullArray}")
 	print(f"Saturated pixels (count==1000): {len(arr1000)/fullArray*100:.3f}% ({len(arr1000)} pixels)")
-	print(f"Noisy pixels (count>100): {noisy/fullArray*100:.3f}% ({noisy} pixels)")
-	
+	print(f"Noisy pixels (count>{threshold}): {noisy/fullArray*100:.3f}% ({noisy} pixels)")
+	print(f"Slightly noisy pixels (0<count<{threshold}): {noisyish/fullArray*100:.3f}% ({noisyish} pixels)")
+
+	#export array of which pixels are masked
+	filename=f"maskedPixels_{loc}_threshold{threshold}.txt"
+	#Organize array so that it matches structure of visualization ((0,0) origin pixel at bottom left)
+	maskArr=np.fliplr(maskArr)
+	maskArr=maskArr.T
+	np.savetxt(filename, maskArr, fmt="%i")#save as ints
