cdadee1319e312f63afa35c264457efe44e54012
Amanda Steinhebel <asteinhebel151@gmail.com>
2022-06-06 13:24:47 -0400

    update scripts

--- analysisScripts/totPlot.py
+++ analysisScripts/totPlot.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 	dirPath=os. getcwd()[:-15] #go one directory above the current one where only scripts are held
 	
 	# For NOISE: Use files of the style *_beamDigital_*.txt (*.log files are only bitstreams)
-	fileIn="logInj/chip1_1min_1.0V_analogPaired_dac16_1.0_injection_20220526-153245.log"
+	fileIn="logInj/chip1_1min_1.8V_analogPaired_dac16_1.8_injection_20220526-153421.log"
 	
 	readFile=ddh.reduceFile(dirPath+fileIn)
 	df=ddh.getDF(readFile)
@@ -35,7 +35,7 @@ if __name__ == "__main__":
 	plt.plot(df['ToT(us)_row']/df['ToT(us)_col'],"o")
 	plt.xlabel('Triggered point')
 	plt.ylabel('Ratio row/col ToT')
-	plt.axhline(y=1, 'r')
+	plt.axhline(y=1, color='r')
 	plt.show()
 	plt.clf()
 	ratio=np.array(df['ToT(us)_row']/df['ToT(us)_col'])
