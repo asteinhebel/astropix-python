81b003e48fd65976f6672bd6e0b561873f2a9816
gs66c235 <gs66c235@ndc.nasa.gov>
2022-07-01 11:14:27 -0400

    Reset default beam_test.py TOT threshold to 100mV

--- beam_test.py
+++ beam_test.py
@@ -61,7 +61,7 @@ def main(args):
 
     # Configure 8 DAC Voltageboard in Slot 4 with list values
     # 3 = Vcasc2, 4=BL, 7=Vminuspix, 8=Thpix
-    vboard1 = Voltageboard(handle, 4, (8, [0, 0, 1.1, 1, 0, 0, 1, 1.175]))
+    vboard1 = Voltageboard(handle, 4, (8, [0, 0, 1.1, 1, 0, 0, 1, 1.1]))
 
     # Set measured 1V for one-point calibration
     vboard1.vcal = 0.989
