aa25ebcfc17cce13543a7154bb0fd7fc09092d99
Amanda Steinhebel <asteinhebel151@gmail.com>
2022-12-08 11:36:10 -0500

    more dynamic searching through file name to get injection voltage - less assumption on how name was created

--- analysisScripts/injScans.py
+++ analysisScripts/injScans.py
@@ -197,10 +197,11 @@ def main(chip):
 	injs=[] #not numerical - defined as files are pulled in. So order of digital data matches, but will not match analog data in combined analog data file
 
 	#get analog data
-	time = 1.8 if chip==602 else 1.9
-	anaDF = adh.getDF(adataDir+'chip'+str(chip)+f'_injScan_10s_combined_0.3Vinj_{time}min.h5py')
-	anaDF.rename(columns={"AnalogToT": "ToT"},inplace=True)
-	dfDict['analog'] = anaDF
+	if analogIn:
+		time = 1.8 if chip==602 else 1.9
+		anaDF = adh.getDF(adataDir+'chip'+str(chip)+f'_injScan_10s_combined_0.3Vinj_{time}min.h5py')
+		anaDF.rename(columns={"AnalogToT": "ToT"},inplace=True)
+		dfDict['analog'] = anaDF
 	
 	#get digital data and injection energies
 	files = glob.glob(os.path.join(ddataDir, f'chip{chip}*.csv'))
@@ -211,28 +212,32 @@ def main(chip):
 		return
 	for f in files:
 		f_nopath = f[lenddir:]
-		injV = f_nopath.split('_')[1][0]
-		#Check whether 10 or 100
-		if len(f_nopath.split('_')[1])==9:
-			injV = 10.
-		injs.append(round(float(injV)*0.1, 2))
+		#find part of file name containing 'inj' which indicates the injection voltage
+		parts = f.split('_')
+		injStr = [p for p in parts if "mVinj" in p]
+		injV = injStr[0][:4] #eliminate 'inj', take first 3 places
+		#Check whether 2 or 3 digit number
+		if injV[-1]=="m":
+			injV = injV[:-1]
+		injs.append(round(float(injV)*0.001, 2))
 		digiDF = ddh.getDF_singlePix(f)
 		dfDict[f'digital_{0.1*float(injV):.1f}'] = digiDF
 	
 	#Analog injection plot
-	analogRuns = separateRuns(dfDict['analog']['Time'],dfDict['analog']['Peaks']) if analogPulseHeight else separateRuns(dfDict['analog']['Time'],dfDict['analog']['ToT'])
-	analogMeans = []
-	analogSigs = []
-	print("Create analog plots")
-	for i,injV in enumerate(analogRuns):
-		mean,sig = fitGauss(injV, (i+1)*0.1, analog=True)
-		analogMeans.append(mean)
-		analogSigs.append(sig)
+	if analogIn:
+		analogRuns = separateRuns(dfDict['analog']['Time'],dfDict['analog']['Peaks']) if analogPulseHeight else separateRuns(dfDict['analog']['Time'],dfDict['analog']['ToT'])
+		analogMeans = []
+		analogSigs = []
+		print("Create analog plots")
+		for i,injV in enumerate(analogRuns):
+			mean,sig = fitGauss(injV, (i+1)*0.1, analog=True)
+			analogMeans.append(mean)
+			analogSigs.append(sig)
 		
-	#AnalogMeans is implicitly sorted in order of increasing injection voltage
-	#injs is defined as files are read in, so not necessarily sorted
-	#Sort injs when plotting for data points to match up appropriately
-	pltInjScan(analogMeans, sorted(injs), analogSigs,"Analog", analog=True)	
+		#AnalogMeans is implicitly sorted in order of increasing injection voltage
+		#injs is defined as files are read in, so not necessarily sorted
+		#Sort injs when plotting for data points to match up appropriately
+		pltInjScan(analogMeans, sorted(injs), analogSigs,"Analog", analog=True)	
 	
 	#Digital injection plot
 	digitalMeans = []
@@ -250,7 +255,7 @@ def main(chip):
 	pltInjScan(digitalMeans_sorted, sorted(injs), digitalSigs_sorted,"Digital (row)")
 		
 	#Compare scans
-	if analogPulseHeight: #compare analog pulse vs analog ToT	
+	if analogIn and analogPulseHeight: #compare analog pulse vs analog ToT	
 		analogToTRuns = separateRuns(dfDict['analog']['Time'],dfDict['analog']['ToT'])
 		analogToTMeans = []
 		analogToTSigs = []
@@ -260,9 +265,11 @@ def main(chip):
 			analogToTSigs.append(sig)
 		print("Comparing analog data")
 		compareAnalogInjScans(analogMeans, analogToTMeans, sorted(injs), analogSigs, analogToTSigs)
-	else: #compare analog and digital ToT
+	elif analogIn: #compare analog and digital ToT
 		print("Comparing analog and digital data")
 		compareInjScans(analogMeans, digitalMeans_sorted, sorted(injs), analogSigs, digitalSigs_sorted)
+	else:
+		print("Only considered digital data - nothing to compare to")
 
 	
 #################################################################
@@ -277,15 +284,23 @@ if __name__ == "__main__":
 	adataDir = "/Users/asteinhe/AstroPixData/astropixOut_tmp/v2/111622_amp1/"
 	chip = [602, 604, 401]
 	"""
-
+	"""
 	#post-irradiation June HI chips
 	saveDir = os.getcwd()+"/plotsOut/lbnlIrradiated/" #hardcode location of dir for saving output plots - automatically saves
 	ddataDir = "/Users/asteinhe/AstroPixData/digital/logInj/lbnl_HIbeam_prepAndPost/"
 	adataDir = "/Users/asteinhe/AstroPixData/astropixOut_tmp/v2/111622_amp1/"
 	chip = [601, 603]
+	"""
+	
+	#high-res chips
+	saveDir = os.getcwd()+"/plotsOut/newChipInjectionScans/HR3/dishidr0/" #hardcode location of dir for saving output plots - automatically saves
+	ddataDir = "/Users/asteinhe/AstroPixData/digital/logInj/injectionScan_chipHR3/"
+	adataDir = "/Users/asteinhe/AstroPixData/astropixOut_tmp/v2/111622_amp1/"
+	chip = ['HR3']
 	
 
-	savePlts = False
+	savePlts = True
+	analogIn = False
 	analogPulseHeight = False #if False, consider analog proxy ToT
 
 	for c in chip:
